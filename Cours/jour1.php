69e1427b63e1594138603853a3bfc7c625d21cb8
Oviglo <ovigne.loic@gmail.com>
2020-03-23 13:56:20 +0100

    Update

---
+++ Cours/jour1.php
@@ -0,0 +1,21 @@
+<?php
+
+// Déclarer une variable
+$prenom = 'Pierre';
+
+// Nombre
+$age = 31;
+// nombre décimal
+$taille = 1.80;
+
+// Booléen
+$estAdmin = true;
+
+/*
+ Une variable ne doit pas commencer par un chiffre
+ $0var -> erreur
+ Peut contenir des underscores _
+ Et des caractères ASCII
+*/
+
+echo $prenom;
