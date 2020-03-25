77e7d3f5b107fef821c5fb212bf20fca6cd89469
Oviglo <ovigne.loic@gmail.com>
2020-03-25 14:24:35 +0100

    function

--- Cours/Jour3/4_function.php
+++ Cours/Jour3/4_function.php
@@ -72,3 +72,51 @@ function salutationPrecise2($name, $mode = 'normal')
 echo '<br/>';
 // On est ici pas obligé de renseigner la valeur $mode, elle sera à 'normal' par défaut
 salutationPrecise2('Jean');
+
+/*
+Les arguments facultatifs doivent êtres placés en dernier
+ function maFonction($param1, $param2 = 0, $param3) {}
+ maFonction("ok", , 100); // Je dois quand même renseigner le param2 pour définir param3
+*/
+
+/**
+ * Si l'utilisateur est admin.
+ */
+function estAdmin($roles)
+{
+    // Si la position de la chaine 'ADMIN' ne retourne pas false
+    // strpos retourne la position de la chaine rechercher, si elle ne la trouve pas elle retourne "false"
+    return false != strpos($roles, 'ADMIN');
+    /*
+    if (false != strpos($roles, 'ADMIN')) {
+        return true;
+    } else {
+        return false;
+    }
+    */
+    // le return arrête le code de la fonction (équivalent du break pour les boucles)
+}
+
+echo '<br/>';
+echo estAdmin('USER,ADMIN') ? 'Vous êtes admin' : 'Vous n\'avez pas les droits';
+
+// une fonction peut retourner une valeur de n'importe quel type
+function getArray()
+{
+    return [
+        'val1' => 'Yes',
+        'val2' => 'No',
+    ];
+}
+
+/*
+Il est possible de typer les paramètres (version 7 de PHP )
+Et de type le retour
+*/
+function connexion(string $username, string $password): bool
+{
+    // Code
+    return false;
+}
+
+connexion('username', 'pass');
---
+++ Cours/Jour3/Exercices/exo_1.php
@@ -0,0 +1,6 @@
+<?php
+    /*
+        1. Créez la fonction `getGreetings` qui renverra le message "Bonjour", sans l'afficher
+
+        2. Utilisez cette fonction pour écrire le message "Bonjour"
+     */
