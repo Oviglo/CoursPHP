ee0f74c1be6664856d7679359d8baa838ea8ab4c
Oviglo <ovigne.loic@gmail.com>
2020-04-01 12:56:31 +0200

    inscription

--- TDCroisiere/src/page/inscription.php
+++ TDCroisiere/src/page/inscription.php
@@ -3,11 +3,19 @@
 $title = "S'inscrire";
 
 $errors = []; // Erreurs pour le formulaire
+$username = '';
+$email = '';
+$password = '';
 
 // Test si le formulaire est envoyé
 if (!empty($_POST)) {
     $_POST = array_map('trim', $_POST);
     $_POST = array_map('strip_tags', $_POST); // retire les balises html
+
+    // Stock les valeurs POST pour les afficher dans les "value" des champs
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
     if (strlen($_POST['username']) < 4) {
         $errors['username'] = "Le nom d'utilisateur est trop court (min: 4 charactères)";
@@ -30,9 +38,9 @@ ob_start();
 <!-- div.card.form-frame>div.card-body>(div.form-group>input:text[name="username"])+(div.form-group>input:email[name="email"])+(div.form-group>input:password[name="password"])+button:submit.btn.btn-primary.btn-block --> 
 <div class="card form-frame">
     <div class="card-body">
-        <form action="" method="post">
+        <form action="" method="post" novalidate>
             <div class="form-group">
-                <input class="form-control <?=isset($errors['username']) ? 'is-invalid' : ''; ?>" placeholder="Nom d'utilisateur" type="text" name="username" id="">
+                <input class="form-control <?=isset($errors['username']) ? 'is-invalid' : ''; ?>" placeholder="Nom d'utilisateur" type="text" name="username" value="<?=$username; ?>">
                 <?php if (isset($errors['username'])): ?>
                 <div class="invalid-feedback">
                     <?=$errors['username']; ?>
@@ -40,10 +48,20 @@ ob_start();
                 <?php endif; ?>
             </div>
             <div class="form-group">
-                <input class="form-control" placeholder="Adresse email" type="email" name="email" id="">
+                <input class="form-control <?=isset($errors['email']) ? 'is-invalid' : ''; ?>" placeholder="Adresse email" type="email" name="email" value="<?=$email; ?>">
+                <?php if (isset($errors['email'])): ?>
+                <div class="invalid-feedback">
+                    <?=$errors['email']; ?>
+                </div>
+                <?php endif; ?>
             </div>
             <div class="form-group">
-                <input class="form-control" placeholder="Mot de passe" type="password" name="password" id="">
+                <input class="form-control <?=isset($errors['password']) ? 'is-invalid' : ''; ?>" placeholder="Mot de passe" type="password" name="password" value="<?=$password; ?>">
+                <?php if (isset($errors['password'])): ?>
+                <div class="invalid-feedback">
+                    <?=$errors['password']; ?>
+                </div>
+                <?php endif; ?>
             </div>
             <button type="submit" class="btn btn-primary btn-block">S'inscrire</button>
         </form>
