5772a909400503e106a591bfd74ada676513d75e
Oviglo <ovigne.loic@gmail.com>
2020-04-06 09:56:39 +0200

    ajax

---
+++ Cours/Ajax/Exemple_1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemple Ajax 1</title>
+</head>
+<body>
+    <button type="button" id="load">Rafraichir la liste</button>
+    <div id="lastNews">
+    </div>
+    <script src="main.js"></script>
+</body>
+</html>
