77a57d9519fa203f0126327809bb5fafa7728729
Oviglo <ovigne.loic@gmail.com>
2020-04-14 15:15:57 +0200

    heritage

---
+++ Cours/PHPOO/Exercices/2_heritage/index.php
@@ -0,0 +1,6 @@
+<?php
+
+/*
+    Ecrire une classe Outil (Outil.php) avec une propriété protected nom et dateAchat (DateTime) avec les setters et getters
+    Ecrire une classe Scie qui hérite de Outil et qui possède une propriété type (pourquoi pas ajouter des contantes TYPE_METAUX, TYPE_BOIS)
+*/
