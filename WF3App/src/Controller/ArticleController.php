dfc4850f720185193855696bd09faf1ea61ea2bb
Oviglo <ovigne.loic@gmail.com>
2020-04-30 12:53:59 +0200

    article form

--- WF3App/src/Controller/ArticleController.php
+++ WF3App/src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -64,24 +65,34 @@ class ArticleController extends AbstractController
     /**
      * @Route("/new")
      */
-    public function new(EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         // Il est possible d'obtenir le service entityManager comme ceci:
         // $entityManager = $this->getDoctrine()->getManager();
 
-        $article = (new Article())
-            ->setTitle('Deuxième article')
-            ->setPublished(true)
-            ->setContent('Apprenez à utiliser Doctrine')
-        ;
+        $article = new Article();
 
-        // Persiste l'objet Article, indique à doctrine qu'on va ajouter un objet (ne fait pas de requête INSERT)
-        $entityManager->persist($article);
+        // Création d'un formulaire et envoi de l'article en data
+        $form = $this->createForm(ArticleType::class, $article);
 
-        // Enregistrement de l'objet (exécute la requête)
-        $entityManager->flush();
+        // Insérer l'objet Request dans le formulaire pour obtenir les données $_POST
+        $form->handleRequest($request);
+
+        // Test si le formulaire a bien été envoyé ET s'il est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Persiste l'objet Article, indique à doctrine qu'on va ajouter un objet (ne fait pas de requête INSERT)
+            $entityManager->persist($article);
+
+            // Enregistrement de l'objet (exécute la requête)
+            $entityManager->flush();
+
+            // redirection
+            return $this->redirectToRoute('app_article_index');
+        }
 
         // NE PAS OUBLIER DE CREER LE FICHIER templates/article/new.html.twig
-        return $this->render('article/new.html.twig');
+        return $this->render('article/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- WF3App/src/Form/ArticleType.php
+++ WF3App/src/Form/ArticleType.php
@@ -4,10 +4,24 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // Ajout des champs dans le formulaire
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('published')
+            // Ajout du submit
+            ->add('save', SubmitType::class)
+        ;
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         // Indique due ce formulaire permet de modifier des objets Article
